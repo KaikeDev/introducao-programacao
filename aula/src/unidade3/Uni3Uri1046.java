a7ac2aec8bf116508c8bf52ffef428d751207f7f
KaikeDev <102434357+KaikeDev@users.noreply.github.com>
2023-03-23 15:30:09 -0300

    novos exercicios

---
+++ aula/src/unidade3/Uni3Uri1046.java
@@ -0,0 +1,31 @@
+package unidade3;
+
+import java.util.Scanner;
+
+public class Uni3Uri1046 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+
+        int horaInicial = scan.nextInt();
+        while (horaInicial < 0) {
+            horaInicial = scan.nextInt();
+        }
+
+        int horaFinal = scan.nextInt();
+
+        while (horaFinal > 24) {
+            horaFinal = scan.nextInt();
+        }
+
+        int duracao = 0;
+        if (horaFinal > horaInicial) {
+            duracao = horaFinal - horaInicial;
+        } else {
+            duracao = (24 - horaInicial) + horaFinal;
+        }
+
+        System.out.println("O JOGO DUROU " +duracao+ " HORA(S)");
+        scan.close();
+    }
+}
---
+++ aula/src/unidade3/Uni3Uri1051.java
@@ -0,0 +1,29 @@
+package unidade3;
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Uni3Uri1051 {
+    public static void main(String[] args) throws Exception {
+
+        Scanner scan = new Scanner(System.in);
+        DecimalFormat f_02 = new DecimalFormat("R$ 0.00");
+
+        double salario = scan.nextDouble();
+
+        if(salario > 0 && salario <=2000){
+            System.out.println("Isento");
+        }else if(salario >2000 && salario <= 3000){
+            double imposto = (salario - 2000) * 0.08;
+            System.out.println(f_02.format(imposto));
+        }else if(salario > 3000 && salario < 4500){
+            double imposto = (salario - 2000) * 0.08;
+            System.out.println(f_02.format(imposto));
+        }else{
+        double imposto = (salario - 2000)* 0.28;
+        System.out.println(f_02.format(imposto));
+        }
+
+        scan.close();
+    }
+}
